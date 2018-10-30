e1f8d4151d1074ac5f777087f91b8d101ed03373
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 14:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ class Driver {
   }
 
 class Route {
-  
-}
-
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+     let eastWestStart = eastWest.indexOf(this.beginningLocation.horizontal);
+    let eastWestEnd = eastWest.indexOf(this.endingLocation.horizontal);
+    let eastWestBlocks = Math.abs(eastWestStart - eastWestEnd);
+     let northSouthStart = this.beginningLocation.vertical
+    let northSouthEnd = this.endingLocation.vertical
+    let northSouthBlocks = Math.abs(northSouthStart - northSouthEnd);
+     return eastWestBlocks + northSouthBlocks;
+  }
+   estimatedTime(isPeakHours = false) {
+    if (isPeakHours) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
 }
