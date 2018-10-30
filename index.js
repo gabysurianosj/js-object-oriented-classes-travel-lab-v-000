a2b5eb5bca9c1475a92384b4830a836e851bc39c
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 14:06:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ class Driver {
     this.startDate = new Date(startDate);
   }
 
-yearsExperienceFromBeginningOf()
+yearsExperienceFromBeginningOf(year) {
+  return year - this.startDate.getFullYear();
+}
 }
