35b08f5083b9092ee98c1932ce0aadc580c81181
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 14:11:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
   }
-
-  yearsExperienceFromBeginningOf(year) {
+   yearsExperienceFromBeginningOf(year) {
     return year - this.startDate.getFullYear();
   }
-
-class Route {
+ }
+ class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
-  blocksTravelled() {
+   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
-    let eastWestStart = eastWest.indexOf(this.beginningLocation.horizontal);
+     let eastWestStart = eastWest.indexOf(this.beginningLocation.horizontal);
     let eastWestEnd = eastWest.indexOf(this.endingLocation.horizontal);
     let eastWestBlocks = Math.abs(eastWestStart - eastWestEnd);
-
-    let northSouthStart = this.beginningLocation.vertical
+     let northSouthStart = this.beginningLocation.vertical
     let northSouthEnd = this.endingLocation.vertical
     let northSouthBlocks = Math.abs(northSouthStart - northSouthEnd);
-
-    return eastWestBlocks + northSouthBlocks;
+     return eastWestBlocks + northSouthBlocks;
   }
    estimatedTime(isPeakHours = false) {
     if (isPeakHours) {
